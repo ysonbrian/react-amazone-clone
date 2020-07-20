d35ae58848008b1a1f0e7bbad2d4f41c333666c5
ysonbrian <znlsakxls@gmail.com>
2020-07-20 21:01:50 +0900

    add some products

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from './Product';
 import './Home.css';
 function Home() {
 	return (
@@ -9,6 +10,57 @@ function Home() {
 				src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg'
 				alt=''
 			/>
+			{/* Product id, title, price, rating, image */}
+			<div className='home_row'>
+				<Product
+					id='12345'
+					title='The New Vacume'
+					price={11.96}
+					rating={5}
+					image='https://www.bigw.com.au/medias/sys_master/images/images/heb/h88/12585601761310.jpg'
+				/>
+				<Product
+					id='4891241'
+					title='Apple MacBook Pro'
+					price={2099.0}
+					rating={5}
+					image='https://m.media-amazon.com/images/I/71L2iBSyyOL._AC_UY436_FMwebp_QL65_.jpg'
+				/>
+			</div>
+			<div className='home_row'>
+				<Product
+					id='51094901'
+					title='Dell S-Series 27 inch monitor'
+					price={374.29}
+					rating={5}
+					image='https://m.media-amazon.com/images/I/81ONemBAVtL._AC_UY436_FMwebp_QL65_.jpg'
+				/>
+				<Product
+					id='5194914'
+					title='Airpods Pro'
+					price={218.45}
+					rating={5}
+					image='https://m.media-amazon.com/images/I/71zny7BTRlL._AC_UY436_FMwebp_QL65_.jpg'
+				/>
+				<Product
+					id='12345'
+					title='Sony Alpha a6000'
+					price={489.0}
+					rating={5}
+					image='https://m.media-amazon.com/images/I/61oKYl9q+kL._AC_UY436_FMwebp_QL65_.jpg'
+				/>
+			</div>
+			<div className='home_row'>
+				<Product
+					id='12345'
+					title='TCL 4K UHD Dolby VISION HDR Roku'
+					price={700.0}
+					rating={5}
+					image='https://m.media-amazon.com/images/I/91ZrvnzneML._AC_UY436_FMwebp_QL65_.jpg'
+				/>
+			</div>
+
+			{/* Product */}
 		</div>
 	);
 }
